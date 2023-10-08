43582217fcf043d34dd23a4fc623859f24aa5ecb
burkecm <burkecm77@gmail.com>
2023-10-08 09:43:41 -0400

    Parse Data, Generate Basic PDFs

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module neutercommuter
 
 go 1.20
+
+require (
+	github.com/gocarina/gocsv v0.0.0-20230616125104-99d496ca653d
+	github.com/jung-kurt/gofpdf v1.16.2
+)
+
+require (
+	github.com/phpdave11/gofpdi v1.0.7 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,87 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
 
+	"github.com/gocarina/gocsv"
+	"github.com/jung-kurt/gofpdf"
+	"github.com/jung-kurt/gofpdf/contrib/gofpdi"
+)
+
+// for when we upgrade to a db rather than csv
+//type Pet struct{
+// 	SpeciesGenderCode string //`csv:"Pet1Code"`
+// 	Age string //`csv:"Pet1Age"`
+// 	WeightEst string //`csv:"Pet1Weight"`
+// 	Name string //`csv:"Pet1Name"`
+// }
+// type Customer struct{
+// 	Own Owner
+// 	Pets []Pet
+// }
+type Owner struct{
+	Name string `csv:"Name"`
+	Phone string `csv:"Phone"`
+	AltPhone string `csv:"AltPhone"`
+	StreetAddress string `csv:"Address"`
+	City string `csv:"City"` 
+	St string `csv:"State"`
+	Zip string `csv:"Zip"`
+	Email string `csv:"Email"`
+	Pet1 string `csv:"Pet1"`
+	Pet2 string `csv:"Pet2"`
+	Pet3 string `csv:"Pet3"`
+	Pet4 string `csv:"Pet4"`
+	Pet5 string `csv:"Pet5"`
+}
+func main() {
+	 bs, err := os.ReadFile("Sample_DataSimple.csv")
+	//bs, err := os.ReadFile(os.Args[1])
+	//startID := strings.TrimSuffix(os.Args[1], ".csv")
+	startID := 1
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	r := csv.NewReader(bytes.NewReader(bs))
+	r.Comment = '/'
+	r.LazyQuotes = true
+	owners := []*Owner{}
+	err = gocsv.UnmarshalCSV(r, &owners)
+	if err != nil && err != io.EOF{
+		log.Fatal(err)
+	}
+	for _, owner := range owners{
+		//fmt.Println(owner)
+		GenerateInvoice(*owner, startID)
+		startID++
+	}
+}
+
+func GenerateInvoice(o Owner, DocID int) error{
+	// generate a new document
+	pdf := gofpdf.New("P", "mm", "A4", "")
+
+	// Import Invoice pdf with gofpdi free pdf document importer
+	tpl1 := gofpdi.ImportPage(pdf, "Voucher_BLANK_TAC_2023_Sample.pdf", 1, "/MediaBox")
+	pdf.AddPage()
+	
+	// Draw imported template onto page
+	gofpdi.UseImportedTemplate(pdf, tpl1, 0, 5, 210, 0)
+
+	// Draw Customer data
+ 	pdf.SetFont("Helvetica", "", 20)
+	pdf.Cell(0, 0,o.Name)
+
+	err := pdf.OutputFileAndClose("Output/Voucher_"+strconv.Itoa(DocID)+".pdf")
+	if err != nil {
+		panic(err)
+	}
+	return nil
 }
